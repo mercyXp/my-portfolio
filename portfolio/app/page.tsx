4dcbac1d1377e221bdcbfe13ad22bcec3c4aaa7c
MercyXp <munzenzimercy2024@gmail.com>
2026-01-31 12:11:05 +0200

    import css/sass/scss modules

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,8 @@
+function Home() {
+  return (
+    <main className="min-h-screen flex flex-col bg-slate-50/50 dark:bg-background">
+
+    </main>
+  );
+}
+export default Home;
---
+++ portfolio/global.d.ts
@@ -0,0 +1,4 @@
+/* This tells TypeScript: "It’s okay to import CSS/SCSS/SASS as a module */
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
